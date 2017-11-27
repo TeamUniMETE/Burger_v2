9cba4bb59e7692443e4ca0bcc7f2b171f6a8b121
RoyalKode <madsoestrem@gmail.com>
2017-11-27 01:44:08 +0100

    added heroku links...comment line until launch

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const db = require('./dbconnect');
 const app = express();
+const path = require('path');
 
 app.use(function(req, res, next) {
     res.set('Access-Control-Allow-Origin', '*');
     res.set('Access-Control-Allow-Methods', 'GET, PUT, POST, DELETE');
     next();
 });
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 
 //----USERROUTE----//
