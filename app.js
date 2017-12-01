c727d33c3c6810168499810ae6f96003c9c1522f
RoyalKode <Madsoestrem@gmail.com>
2017-12-01 18:33:16 +0100

    final

--- app.js
+++ app.js
@@ -30,26 +30,6 @@ var tasks = require('./tasks');
 app.use('/tasks', tasks);
 
 
-//-------------------------//
-app.get('/', function(req, res) {
-
-    res.send('ROOT');
-});
-
-app.get('/test', function(req, res) {
-
-    var sql = 'SELECT * FROM users';
-
-    db.any(sql).then(function(data) {
-
-        res.status(200).json(data);
-
-    }).catch(function(err) {
-
-        res.status(500).json({err});
-    });
-});
-
 app.listen(process.env.PORT || 3000, function (){
   console.log('server is running on port %d in %s mode', this.address().port, app.settings.env);
 });
